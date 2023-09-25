f4508d1af7a7c7f11016679fcee874823c607829
RubenRuiz411 <rubenmruiz.411@gmail.com>
2023-09-24 22:35:20 -0700

    trying to fix npm issues

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -67,7 +67,7 @@ const Home = () => {
             <AnimatedLetters
               letterClass={letterClass}
               strArray={jobArray}
-              idx={15}
+              idx={14}
             />
           </h1>
           <h2>Full Stack Developer / JavaScript Expert </h2>
